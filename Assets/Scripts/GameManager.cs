2ceec43781ffb4d3ee40ef66ea4c573e1d616029
Kla <naphat.watt@bumail.net>
2023-11-22 11:03:44 +0700

    Hole Added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,11 +8,20 @@ public class GameManager : MonoBehaviour
     public int PlayerScore { get => playerScore; set => playerScore = value;}
     [SerializeField] private Ball ballPrefab;
     [SerializeField] private GameObject[] ballPosition;
+    [SerializeField] private GameObject ballLine;
+    [SerializeField] private GameObject cueBall;
+    [SerializeField] private float xInput;
+    [SerializeField] private GameObject camera;
+    public TMP_Text playerScoreText;
+
     public static GameManager instant;
     
     void Start()
     {
         instant = this;
+        camera = Camera.main.gameObject;
+        CameraBehindBueBall();
+        camera.transform.SetParent(cueBall.transform);
         SetBall(BallColor.White,0);
         SetBall(BallColor.Red,1);
         SetBall(BallColor.Yellow,2);
@@ -25,12 +35,49 @@ public class GameManager : MonoBehaviour
     
     void Update()
     {
+        RotateBall();
+        if (Input.GetKeyDown(KeyCode.Space))
+            ShootBall();
         
+        if (Input.GetKeyDown(KeyCode.Backspace))
+            StopBall();
     }
 
     private void SetBall(BallColor col, int i)
     {
         Ball ball = Instantiate(ballPrefab,ballPosition[i].transform.position,Quaternion.identity);
         ball.SetColorAndPoint(col);
+    }
+    
+    private void RotateBall()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        cueBall.transform.Rotate(new Vector3(0f, xInput , 0f));
+    }
+
+    private void ShootBall()
+    {
+        camera.transform.parent = null;
+        Rigidbody rb = cueBall.GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * 50,ForceMode.Impulse);
+        ballLine.SetActive(false);
+    }
+
+    private void CameraBehindBueBall()
+    {
+        camera.transform.parent = cueBall.transform;
+        camera.transform.position = cueBall.transform.position + new Vector3(0f, 7f, -10f);
+    }
+
+    private void StopBall()
+    {
+        Rigidbody rb = cueBall.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        cueBall.transform.eulerAngles = Vector3.zero;
+        
+        CameraBehindBueBall();
+        camera.transform.eulerAngles = new Vector3(30f, 0f, 0f);
+        ballLine.SetActive(true);
     }
 }
---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+   private void OnCollisionEnter(Collision collision)
+   {
+      Ball b = collision.gameObject.GetComponent<Ball>();
+
+      if (b)
+      {
+         GameManager.instant.PlayerScore += b.Point;
+         GameManager.instant.playerScoreText.text = $"Score={GameManager.instant.PlayerScore}";
+         Destroy(b.gameObject);
+      }
+   }
+}
